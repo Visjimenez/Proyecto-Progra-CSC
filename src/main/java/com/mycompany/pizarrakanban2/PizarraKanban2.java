8b2a7d35d8c1c6f183c6360b34cf0d55349b4cfc
Visjimenez <eliasjimenezmendez28@gmail.com>
2025-10-19 17:37:57 -0600

    CAMBIOS

---
+++ src/main/java/com/mycompany/pizarrakanban2/PizarraKanban2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pizarrakanban2;
+
+/**
+ *
+ * @author elias
+ */
+public class PizarraKanban2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
